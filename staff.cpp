2f9ea4b3f682db8606ed46b2da010e937eb20e53
Le Hoang <83994545+hoangle1472002@users.noreply.github.com>
2021-10-23 11:41:45 +0700

    Update staff.cpp

--- staff.cpp
+++ staff.cpp
@@ -1,11 +1,5 @@
 #include "staff.h"
-Staff::Staff(string IDStaff, string Staffname, Date Daystart, string Sex, double Salary)
+Staff::~Staff()
 {
-    this->IDStaff = IDStaff;
-    this->Staffname = Staffname;
-    this->Daystart = Daystart;
-    this->Sex = Sex;
-    this->Salary = Salary;
+    
 }
-Staff::Staff(){}
-Staff::~Staff(){}
