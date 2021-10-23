23d92eb638d1101c46120a0a07c3d186b20a9215
Le Hoang <83994545+hoangle1472002@users.noreply.github.com>
2021-10-23 11:40:40 +0700

    Update date.cpp

--- date.cpp
+++ date.cpp
@@ -1,5 +1,38 @@
 #include "date.h"
-Date::Date(){}
+#include<time.h>
+#include<ctime>
+using namespace std;
+Date::Date()
+{
+    
+}
 Date::Date(int day, int month, int year)
-    : day(day), month(month), year(year) {}
-Date::~Date() {}
+    : day(day), month(month), year(year)
+{
+
+}
+Date::~Date()
+{
+
+}
+
+int Date::TinhNgayLamViecThangHTai()
+{
+
+}
+
+ostream& operator<<(ostream& out,const Date& d)
+{
+    out<<d.day<<"/"<<d.month<<"/"<<d.year<<endl;
+    return out;
+}
+istream& operator>>(istream& in,Date& d)
+{
+    cout<<"Enter day : ";
+    in>>d.day;
+    cout<<"Enter month : ";
+    in>>d.month;
+    cout<<"Enter year : ";
+    in>>d.year;
+    return in;
+}
