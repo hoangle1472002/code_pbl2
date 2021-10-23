3eb2742653c3012e4e9aaaa893aeee12bc49124f
Le Hoang <83994545+hoangle1472002@users.noreply.github.com>
2021-10-23 11:40:03 +0700

    Update NVHD.h

--- NVHD.h
+++ NVHD.h
@@ -1,11 +1,16 @@
 #include "Staff.h"
-class Staff_NVHD: public Staff
+class NVHD: public Staff
 {
 private:
     double Salary_day;
 public:
-    Staff_NVHD(double =0);
-    ~Staff_NVHD();
-    double get_salary();
-    double set_salary(double,int=0);
+    NVHD();
+    NVHD(string,string,Date,bool,double);
+    ~NVHD();
+
+    friend ostream& operator<<(ostream out,NVHD& nv);
+    friend istream& operator>>(istream in,NVHD& nv);
+
+private:
+    virtual void kindof_Staff();
 };
